dcca4c457ea6d278a52ea638d262ce70a746132f
onlycan17 <onlycan17@gmail.com>
2021-08-27 21:10:13 +0900

    캘린더 기능 대부분 추가

--- src/controller/scheduleController.js
+++ src/controller/scheduleController.js
@@ -7,7 +7,10 @@ export const getSchedule = async (req, res) => {
   let schedule;
   url = req.url;
   const now = new Date();
-  const dateMonth = now.getFullYear() + "-" + (now.getMonth() + 1);
+  const dateMonth =
+    now.getFullYear() +
+    "-" +
+    (now.getMonth() + 1 < 10 ? "0" + (now.getMonth() + 1) : now.getMonth() + 1);
   console.log(dateMonth);
   //console.log(JSON.stringify(req.session.user.department._id));
   //관리자일 경우
@@ -25,22 +28,18 @@ export const getSchedule = async (req, res) => {
     const department = subMenu.department[0];
     console.log(department);
     schedule = await Schedule.find({
-      department,
-      $or: [
-        { start: new RegExp(dateMonth, "i") },
-        { end: new RegExp(dateMonth, "i") },
-      ],
+      //department,
+      $or: [{ start: new RegExp(dateMonth) }, { end: new RegExp(dateMonth) }],
     });
   } else {
     schedule = await Schedule.find({
       department: req.session.user.department,
-      $or: {
-        start,
-      },
+      $or: [{ start: new RegExp(dateMonth) }, { end: new RegExp(dateMonth) }],
     });
   }
+  console.log(schedule);
   const color = req.session.user.color;
-  console.log(color);
+  //console.log(color);
   return res.render("schedule", { pageTitle: "스케줄 샘플", schedule, color });
 };
 
@@ -51,7 +50,6 @@ const isUrl = (element, index) => {
 };
 
 export const postAddSchedule = async (req, res) => {
-  console.log("DDDDDDDDDD");
   const {
     title,
     description,
@@ -63,22 +61,74 @@ export const postAddSchedule = async (req, res) => {
     user,
     department,
   } = req.body;
-  try{
-    const schedule = await Schedule.create({
-      title,
-      description,
-      url,
-      start,
-      end,
-      allDay,
-      color,
-      user,
-      department,
-    });
-    return res.sendStatus(200);
-  }catch(error){
-    console.log(error);
-    return res.sendStatus(400);
-  }
-  
+  console.log(title, description);
+  let schedule;
+
+  schedule = await Schedule.create({
+    title,
+    description,
+    url,
+    start,
+    end,
+    allDay,
+    color,
+    user,
+    department,
+  });
+  console.log(schedule);
+
+  return res.sendStatus(201);
 };
+
+export const deleteSchedule = async (req, res) => {
+  console.log('deleteSchedule~~~!');
+  console.log(req.body);
+  const {id} = req.body;
+  const result = await Schedule.findOneAndDelete(id);
+  return res.sendStatus(200);
+};
+
+export const customSchedule = async (req, res) => {
+  let schedule;
+  //url = req.url;
+  //console.log(req);
+  const {url, monthCaculate } = req.query;
+  console.log(req.query);
+  console.log(url);
+  const now = new Date();
+  const dateMonth =
+    now.getFullYear() +
+    "-" +
+    (now.getMonth()+ Number(monthCaculate) + 1 < 10 ? "0" + (now.getMonth()+ Number(monthCaculate) + 1) : now.getMonth() + Number(monthCaculate) + 1);
+  console.log(dateMonth);
+  //console.log(JSON.stringify(req.session.user.department._id));
+  //관리자일 경우
+  if (req.session.user.department._id === "612490cc21f010838f50a41b") {
+    const menu = await Menu.findOne({
+      subMenu: {
+        $elemMatch: {
+          subMenuUrl: url,
+        },
+      },
+    }).populate("subMenu");
+    console.log(menu);
+    const subMenu = await menu.subMenu.find(isUrl);
+    const department = subMenu.department[0];
+    console.log(department);
+    schedule = await Schedule.find({
+      //department,
+      $or: [{ start: new RegExp(dateMonth) }, { end: new RegExp(dateMonth) }],
+    });
+  } else {
+    schedule = await Schedule.find({
+      department: req.session.user.department,
+      $or: [{ start: new RegExp(dateMonth) }, { end: new RegExp(dateMonth) }],
+    });
+  }
+  console.log(schedule);
+  const color = req.session.user.color;
+  //console.log(color);
+  return res.status(200).json({
+    schedule
+  });
+}
