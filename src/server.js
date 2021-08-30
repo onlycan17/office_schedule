998791e341ed2ec4673554b4531c86bbdecd5324
onlycan17 <onlycan17@gmail.com>
2021-08-30 14:10:57 +0900

    config

--- src/server.js
+++ src/server.js
@@ -18,20 +18,20 @@ const ips = ['127.126.0.1'];
 
 app.set("view engine","pug");
 app.set("views",process.cwd()+"/src/views/");
-app.use(ipfilter(ips));
-app.use(function(err, req, res, _next) {
-    //console.log('Error handler', err);
-    res.send('접속이 차단되었습니다. 관리자에게 문의하세요.');                     // page view 'Access Denied'
-    if(err instanceof IpDeniedError){
-      res.status(401).end();
-    }else{
-      res.status(err.status || 500).end();
-    }
-    // res.render('error', {
-    //   message: 'You shall not pass',
-    //   error: err
-    // });
-});
+// app.use(ipfilter(ips));
+// app.use(function(err, req, res, _next) {
+//     //console.log('Error handler', err);
+//     res.send('접속이 차단되었습니다. 관리자에게 문의하세요.');                     // page view 'Access Denied'
+//     if(err instanceof IpDeniedError){
+//       res.status(401).end();
+//     }else{
+//       res.status(err.status || 500).end();
+//     }
+//     // res.render('error', {
+//     //   message: 'You shall not pass',
+//     //   error: err
+//     // });
+// });
 app.use(express.urlencoded({extends: true}));
 app.use(express.json());
 app.use(
