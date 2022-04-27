e4e33b518dc6cfbf2994ab8c3e3de576fdacf75b
onlycan17 <onlycan17@gmail.com>
2022-04-27 19:14:12 +0900

    팜보이스 메뉴 추가

--- assets/js/join.js
+++ assets/js/join.js
@@ -1,1 +1,32 @@
-(()=>{function e(e,n,a){return n in e?Object.defineProperty(e,n,{value:a,enumerable:!0,configurable:!0,writable:!0}):e[n]=a,e}function n(){var n,t={departmentId:$("#partId option:selected").val(),userName:$("#name").val(),email:$("#email").val()};console.log(t),$("#join").DataTable({paging:!0,pageLength:10,processing:!0,serverSide:!0,destroy:!0,searching:!1,language:a,ajax:(n={data:"json",type:"post",url:"/joinList"},e(n,"data",t),e(n,"dataSrc",(function(e){return console.log(e),e.data})),n),columnDefs:[{targets:[0],orderable:!0,searchable:!1,render:function(e,n,a,t){return'<a href="/join/'.concat(a._id,'">').concat(a.name,"</a>")}},{targets:[1],orderable:!1,searchable:!1,render:function(e,n,a,t){return'<a href="/join/'.concat(a._id,'">').concat(a.email,"</a>")}},{targets:[3],orderable:!1,searchable:!1,render:function(e,n,a,t){return console.log(a),a.position?a.position:""}},{targets:[4],orderable:!1,searchable:!1,render:function(e,n,a,t){return console.log(a),'<input type="color" disabled value="'.concat(a.color,'" />')}}],columns:[{data:"name",title:"성명"},{data:"email",title:"이메일"},{data:"departmentName",title:"부서"},{data:"position",title:"직책"},{data:"color",title:"컬러"}],error:function(e,n,a){console.log(e),console.log(a)}})}$(document).ready((function(){$("#searchBtn").click((function(){n()})),$("#partId").change((function(){n()})),$("#name").keydown((function(e){13==e.keyCode&&n()})),$("#email").keydown((function(e){13==e.keyCode&&n()}))}));var a={decimal:"",emptyTable:"데이터가 없습니다.",info:"_START_ - _END_ (총 _TOTAL_ 건)",infoEmpty:"0건",infoFiltered:"(전체 _MAX_ 건 중 검색결과)",infoPostFix:"",thousands:",",lengthMenu:"_MENU_ 개씩",loadingRecords:"로딩중...",processing:"처리중...",search:"검색 : ",zeroRecords:"검색된 데이터가 없습니다.",paginate:{first:"첫 페이지",last:"마지막 페이지",next:"다음",previous:"이전"},aria:{sortAscending:" :  오름차순 정렬",sortDescending:" :  내림차순 정렬"}}})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/client/js/join.js":
+/*!*******************************!*\
+  !*** ./src/client/js/join.js ***!
+  \*******************************/
+/***/ (() => {
+
+eval("function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }\n\n$(document).ready(function () {\n  $(\"#searchBtn\").click(function () {\n    tableSearch();\n  });\n  $(\"#partId\").change(function () {\n    tableSearch();\n  });\n  $(\"#name\").keydown(function (key) {\n    if (key.keyCode == 13) {\n      tableSearch();\n    }\n  });\n  $(\"#email\").keydown(function (key) {\n    if (key.keyCode == 13) {\n      tableSearch();\n    }\n  });\n});\n\nfunction tableSearch() {\n  var _ajax;\n\n  var form_data = {\n    departmentId: $(\"#partId option:selected\").val(),\n    userName: $(\"#name\").val(),\n    email: $(\"#email\").val()\n  };\n  console.log(form_data);\n  $(\"#join\").DataTable({\n    paging: true,\n    pageLength: 10,\n    processing: true,\n    serverSide: true,\n    destroy: true,\n    searching: false,\n    language: lang_kor,\n    ajax: (_ajax = {\n      data: \"json\",\n      type: \"post\",\n      url: \"/joinList\"\n    }, _defineProperty(_ajax, \"data\", form_data), _defineProperty(_ajax, \"dataSrc\", function dataSrc(res) {\n      console.log(res);\n      return res.data;\n    }), _ajax),\n    columnDefs: [{\n      targets: [0],\n      orderable: true,\n      searchable: false,\n      render: function render(data, type, row, meta) {\n        // console.log('----------------');\n        // console.log(row);\n        return \"<a href=\\\"/join/\".concat(row._id, \"\\\">\").concat(row.name, \"</a>\");\n      }\n    }, {\n      targets: [1],\n      orderable: false,\n      searchable: false,\n      render: function render(data, type, row, meta) {\n        // console.log('----------------');\n        // console.log(row);\n        return \"<a href=\\\"/join/\".concat(row._id, \"\\\">\").concat(row.email, \"</a>\");\n      }\n    }, {\n      targets: [3],\n      orderable: false,\n      searchable: false,\n      render: function render(data, type, row, meta) {\n        //console.log('----------------');\n        console.log(row);\n        return row.position ? row.position : '';\n      }\n    }, {\n      targets: [4],\n      orderable: false,\n      searchable: false,\n      render: function render(data, type, row, meta) {\n        //console.log('----------------');\n        console.log(row);\n        return \"<input type=\\\"color\\\" disabled value=\\\"\".concat(row.color, \"\\\" />\");\n      }\n    }],\n    columns: [{\n      data: \"name\",\n      title: \"성명\"\n    }, {\n      data: \"email\",\n      title: \"이메일\"\n    }, {\n      data: \"departmentName\",\n      title: \"부서\"\n    }, {\n      data: \"position\",\n      title: \"직책\"\n    }, {\n      data: \"color\",\n      title: \"컬러\"\n    }],\n    error: function error(xhr, _error, code) {\n      console.log(xhr);\n      console.log(code);\n    }\n  });\n}\n\nvar lang_kor = {\n  decimal: \"\",\n  emptyTable: \"데이터가 없습니다.\",\n  info: \"_START_ - _END_ (총 _TOTAL_ 건)\",\n  infoEmpty: \"0건\",\n  infoFiltered: \"(전체 _MAX_ 건 중 검색결과)\",\n  infoPostFix: \"\",\n  thousands: \",\",\n  lengthMenu: \"_MENU_ 개씩\",\n  loadingRecords: \"로딩중...\",\n  processing: \"처리중...\",\n  search: \"검색 : \",\n  zeroRecords: \"검색된 데이터가 없습니다.\",\n  paginate: {\n    first: \"첫 페이지\",\n    last: \"마지막 페이지\",\n    next: \"다음\",\n    previous: \"이전\"\n  },\n  aria: {\n    sortAscending: \" :  오름차순 정렬\",\n    sortDescending: \" :  내림차순 정렬\"\n  }\n};\n\n//# sourceURL=webpack://masterpiece_site/./src/client/js/join.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/client/js/join.js"]();
+/******/ 	
+/******/ })()
+;
